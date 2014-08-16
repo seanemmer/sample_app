8f62f9174ccb69e4edb509e8d50cd4623ff935d9
Sean Emmer <sean.emmer@gmail.com>
2014-08-15 21:27:00 -0400

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -96,6 +96,19 @@ describe "Authentication" do
           it { should have_title('Sign in') }
         end
       end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
 
     describe "as wrong user" do
@@ -148,7 +161,7 @@ describe "Authentication" do
 
       describe "submitting a DELETE request on self" do
         specify do
-          expect{ delete user_path(admin) }.not_to change(User, :count).by(-1)
+          expect{ delete user_path(admin) }.not_to change(User, :count)
         end
       end
     end
